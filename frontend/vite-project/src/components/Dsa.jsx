1d37926cc2e11f6b257dacba697413433e8b0258
pranay nautiyal <pranaynautiyal462@gmail.com>
2026-03-01 00:46:23 +0530

    updated dsa

--- frontend/vite-project/src/components/Dsa.jsx
+++ frontend/vite-project/src/components/Dsa.jsx
@@ -1,51 +1,90 @@
-import React from 'react'
-import './Dsa.css'
+
+import React from 'react';
+import './Dsa.css';
+import { FaMicrophone } from 'react-icons/fa';
+
 const Dsa = () => {
     return (
-        <div className="dsa-main">
+        <div className='dsa-main'>
 
-            {/* TOP SECTION - 30% */}
-            <div className="top">
+            {/* TOP 10% */}
+            <div className="dsa-top">
+                <div className="dsa-header">
+                    <h1>DSA-PREP</h1>
+                </div>
+
+                <div className="dsa-audio">
+                    <button className="mic-button" title="Start/Stop Recording">
+                        <FaMicrophone />
+                    </button>
+                </div>
+            </div>
+
+            {/* MIDDLE 80% */}
+            <div className="dsa-middle">
+
                 <div className="dsa-question">
                     <h3>Question</h3>
                     <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Vel, adipisci sequi reprehenderit autem possimus ea rem
-                        voluptate? Aliquam, sint officia! Mollitia necessitatibus
-                        voluptatibus consequuntur nam sequi porro, doloribus nesciunt autem!
+                        Write a function to find the longest increasing
+                        subsequence in an array.ghvssassSasASASADQWdssxsxsascsdadxasdxsadsa
+                        xXaxaazzzzzzzzzzzzzzzzzz
+                        assaasa
+                        asasasssssss<br></br>
+                        ccf<br></br>
+                        cdc<br></br>
+                        fcv<br></br>
+                        fvv<br></br>
+                        fvfv
+                        ccf<br></br>
+                        cdc<br></br>
+                        fcv<br></br>
+                        fvv<br></br>
+                        ccf<br></br>
+                        cdc<br></br>
+                        fcv<br></br>
+                        fvv<br></br>
+                        ccf<br></br>
+                        cdc<br></br>
+                        fcv<br></br>
+                        fvv<br></br>
+                        ccf<br></br>
+                        cdc<br></br>
+                        fcv<br></br>
+                        fvv<br></br>
+                        ccf<br></br>
+                        cdc<br></br>
+                        fcv<br></br>
+                        fvv<br></br>
+                        ccf<br></br>
+                        cdc<br></br>
+                        fcv<br></br>
+                        fvv<br></br>
+                        ccf<br></br>
+                        cdc<br></br>
+                        fcv<br></br>
+                        fvv<br></br>
+
                     </p>
                 </div>
 
-                <div className="microphone-button">
-                    <button>🎤</button>
-                </div>
-            </div>
-
-            {/* MIDDLE SECTION - 60% */}
-            <div className="middle">
                 <div className="dsa-code">
-                    <textarea placeholder="Write your code here..."></textarea>
+                    <textarea placeholder='Type Your Code Here'></textarea>
                 </div>
 
-                <div className="performance-report">
-                    <h3>Performance Report</h3>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Reprehenderit iure saepe sit non aut accusantium.
-                        Corporis quae ipsum similique!
-                    </p>
-                </div>
             </div>
 
-            {/* BOTTOM SECTION - 10% */}
-            <div className="bottom">
-                <div className="audioToText">
-                    <p>Voice input will appear here...</p>
+            {/* BOTTOM 10% */}
+            <div className="dsa-bottom">
+
+                <div className="audio-text">
+                    Voice transcript will appear here...
                 </div>
 
-                <div className="submit">
-                    <button type="submit">Submit</button>
+                <div className="dsa-button">
+                    <button>Submit and Evaluate</button>
                 </div>
+
             </div>
 
         </div>
