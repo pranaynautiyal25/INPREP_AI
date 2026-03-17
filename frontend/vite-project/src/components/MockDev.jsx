4326acd57f27332136a81ea14d41b4067fb10b8e
pranay nautiyal <pranaynautiyal462@gmail.com>
2026-03-18 00:19:05 +0530

    slight changes

--- frontend/vite-project/src/components/MockDev.jsx
+++ frontend/vite-project/src/components/MockDev.jsx
@@ -11,7 +11,7 @@ const MockDev = () => {
 
     return (
         <div style={{ color: 'white' }}>
-            <p>Exam Category: {info.category}</p>
+            {/* <p>Exam Category: {info.category}</p>
             <br></br>
             <p>Your Answer: {info.yourAnswer}</p>
             <br></br>
@@ -21,7 +21,46 @@ const MockDev = () => {
             <br></br>
             <p>Explaination Score: {info.explainationScore}</p>
             <br></br>
-            <p>Improvement Scope: {info.improvementScope}</p>
+            <p>Improvement Scope: {info.improvementScope}</p> */}
+
+            <div className='devHist-top'>
+                <div>
+                    <h1><b><span>{info.category}</span> HISTORY</b></h1>
+                </div>
+                <div>
+                    <button>Back</button>
+                </div>
+            </div>
+
+            <div className='devHist-middle'>
+                <div>
+                    {info.question}
+                </div>
+                <div className='devHist-left'>
+                    {info.yourAnswer}
+                </div>
+                <div className='devHist-right'>
+                    {info.correctAnswer}
+                </div>
+            </div>
+
+            <div className='devHist-bottom'>
+                <div>
+                    <div>
+                        {info.answerScore}
+                    </div>
+                    <div>
+                        {info.explainationScore}
+                    </div>
+                </div>
+                <div>
+                    <div>
+                         {info.improvementScope}
+                    </div>
+                </div>
+
+            </div>
+
 
         </div>
     )
